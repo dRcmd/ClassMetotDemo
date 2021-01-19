bed51252c47b258d24b3f8678198f6e943ae3a23
dRcmd <76197823+dRcmd@users.noreply.github.com>
2021-01-19 22:59:53 +0300

    update

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(int id, string firstName, string lastName, decimal kasa)
+        {
+            new Musteri()
+            {
+                Id = id,
+                FirstName = firstName,
+                LastName = lastName,
+                Kasa = kasa
+            };
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,8 +6,17 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            MusteriManager musteriManager = new MusteriManager();
+            void[] musteriler = new void[]
+            {
+                musteriManager.Ekle(1, "Ahmet", "Kutluay", 100.0m);
+                musteriManager.Ekle(2, "Mustafa", "Kutluay", 100.0m);
+                musteriManager.Ekle(3, "Ali", "Kutluay", 100.0m);
+
+        };
+           
+            
+
         }
     }
 }
